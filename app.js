43750f6550ee1a71321c1b53b137e0ea1e179f7a
Rosario <rosariogomez639@gmail.com>
2022-08-04 13:22:35 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let input = prompt("What would you like to do?");
+
+while (input !== 'quit' && input !== 'q') {
+    input = prompt("What would you like to do?");
+}
+
+console.log("OK, Quit de the app"); 
